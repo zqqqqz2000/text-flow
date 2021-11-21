cba1c27d54adc3b1a1c3ff3ae1c293acf88f066a
zqqqqz2000 <zqqqqz2000@sina.cn>
2021-11-22 01:34:41 +0800

    feat: drag and drop

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,9 +1,9 @@
-import { Center, Stack } from '@chakra-ui/layout';
+import React, { memo, useEffect, useState, DragEvent, useRef } from 'react';
+import { Center, Square, Stack } from '@chakra-ui/layout';
 import { StackDivider } from '@chakra-ui/react';
-import React, { memo, useEffect, useMemo, useState } from 'react';
 import AceEditor from 'react-ace';
 import "ace-builds/src-noconflict/ext-language_tools";
-import ReactFlow, { Controls, Elements as FlowElements, FlowElement, Handle, MiniMap, Position } from 'react-flow-renderer';
+import ReactFlow, { Controls, Elements as FlowElements, FlowElement, Handle, MiniMap, OnLoadParams, Position } from 'react-flow-renderer';
 import { FlowProcessor, ProcessorChainNode } from '../utils/flowProcessor';
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-tomorrow";
@@ -14,25 +14,56 @@ export type TextFlowTextProps = {
 };
 
 const SideBar: React.FC = () => {
-    return <Stack w="300px" height="100%">
-
+    const onDragStart = (event: DragEvent, nodeType: string) => {
+        const dataTransfer = event?.dataTransfer;
+        if (dataTransfer) {
+            event.dataTransfer.setData('application/reactflow', nodeType);
+            event.dataTransfer.effectAllowed = 'move';
+        }
+    };
+    return <Stack w="300px" height="100%" spacing={4} p={2}>
+        <Square p={2} border="1px solid black" onDragStart={(event) => onDragStart(event, 'map')} draggable>
+            Map
+        </Square>
+        <Square p={2} border="1px solid black" onDragStart={(event) => onDragStart(event, 'filter')} draggable>
+            Filter
+        </Square>
+        <Square p={2} border="1px solid black" onDragStart={(event) => onDragStart(event, 'reduce')} draggable>
+            Reduce
+        </Square>
+        <Square p={2} border="1px solid black" onDragStart={(event) => onDragStart(event, 'join')} draggable>
+            Join
+        </Square>
+        <Square p={2} border="1px solid black" onDragStart={(event) => onDragStart(event, 'process')} draggable>
+            Process
+        </Square>
     </Stack>
 };
 
-const TextInputNode: React.FC<{ data: {}, isConnectable: boolean }> = memo(({ data, isConnectable }) => {
+const TextInputNode: React.FC<{ data: {}, isConnectable: boolean }> = memo(({ data }) => {
     return <>
         <Center h="100%">
             Text Source<br />
             Split Function
         </Center>
+        <Handle
+            type="source"
+            position={Position.Right}
+            id="source-output"
+            isConnectable={true}
+        />
     </>;
 });
 
+let id = 0;
+const getId = () => `processor-${id++}`;
 
 export const TextFlow: React.FC<TextFlowTextProps> = ({ flowProcessor }) => {
     const [elements, setElements] = useState<FlowElements>([]);
     const [nodeElementInfo, setNodeElementInfo] = useState<Map<ProcessorChainNode, FlowElement>>(new Map());
     const [currentElement, setCurrentElement] = useState<FlowElement | undefined>(undefined);
+    const reactFlowWrapper = useRef<HTMLDivElement>(null);
+    const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams | undefined>(undefined);
     useEffect(() => {
         const newElements: FlowElements = [];
         let rootElement = nodeElementInfo.get(flowProcessor.processChain);
@@ -50,6 +81,32 @@ export const TextFlow: React.FC<TextFlowTextProps> = ({ flowProcessor }) => {
         newElements.push(rootElement);
         setElements(newElements);
     }, [flowProcessor, nodeElementInfo]);
+    const onLoad = (_reactFlowInstance: OnLoadParams) =>
+        setReactFlowInstance(_reactFlowInstance);
+
+    const onDragOver = (event: DragEvent) => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+    };
+    const onDrop = (event: DragEvent) => {
+        event.preventDefault();
+        if (reactFlowWrapper.current && reactFlowInstance) {
+            const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+            const type = event.dataTransfer.getData('application/reactflow');
+            const position = reactFlowInstance.project({
+                x: event.clientX - reactFlowBounds.left,
+                y: event.clientY - reactFlowBounds.top ?? 0,
+            });
+            const newNode = {
+                id: getId(),
+                type,
+                position,
+                data: { label: `${type} node` },
+            };
+
+            setElements((es) => es.concat(newNode));
+        }
+    };
     return <Stack
         h="100%"
         w="100%"
@@ -79,6 +136,10 @@ export const TextFlow: React.FC<TextFlowTextProps> = ({ flowProcessor }) => {
         <SideBar />
         <ReactFlow
             snapToGrid={true}
+            onLoad={onLoad}
+            onDragOver={onDragOver}
+            ref={reactFlowWrapper}
+            onDrop={onDrop}
             snapGrid={[10, 20]}
             onElementClick={(_, element) => { setCurrentElement(element) }}
             defaultZoom={0.7}
